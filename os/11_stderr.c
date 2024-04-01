b61cec0942d2aacb40288242952137be9dc1e65c
enderbuster <mcenderman@163.com>
2024-04-01 14:18:47 +0800

    we use 1> to redirect stdout,we use 2> to redirect stderr in linux command line

--- os/11_stderr.c
+++ os/11_stderr.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 //through this program,see the use of stderr and stdin
 void main(){
-	//stdin
+	//stdout
 	printf("use printf to print in stdout\n");
-	//stdin
+	//stdout
 	fprintf(stdout,"use fprintf to print in stdout\n");
-	//std
+	//stderr
 	fprintf(stderr,"use fprintf to print in stderr\n");
 }
 

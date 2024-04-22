32e915a7190fd6b2072f91660d7e38f91d8becf3
enderbuster <mcenderman@163.com>
2024-04-22 19:23:44 +0800

    a/(b+0.0)to get float

---
+++ c_cookbook/01_int_to_float.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main(){
+	//a and b are both type of int
+	int a=3;
+	int b=4;
+	float res=a/b;
+	printf("a/b=%f\n",res);
+	//the result is 0.000
+	//to get a result of float by using +0.0
+	float correct_res=a/(b+0.0);
+	printf("a/b=%f\n",correct_res);
+	//now the result is
+	
+}
